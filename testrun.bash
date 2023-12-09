2539f3f9e1cacaac415800f8f489cf6108538cd5
Marc Gilligan <marcg@ulfnic.com>
2023-12-09 22:43:02 +0000

    feat: add --app-root-dir and various defaults if the script's directory is named 'tests'

--- testrun.bash
+++ testrun.bash
@@ -10,21 +10,26 @@ help_doc() {
 
 		testrun.sh [OPTION]... [test-FILE]... [DIRECTORY]...
 
-		A generic stand-alone script for handling execution and feedback for test files.
+		A stand-alone script for running tests intended to be included inline within projects or installed on a host system.
 
-		FILEs must be executable and begin with 'test-'
+		test-FILEs must be executable and begin with 'test-', each DIRECTORY is searched recursively for test-FILEs
 
-		Each DIRECTORY is searched recursively for executable FILEs beginning with 'test-'
+		If the directory containing this script is named 'tests':
+		  - --app-root-dir defaults to the parent of the directory containing this script (one level up).
+		  - Test paths default to the sub-directories of the directory containing this script.
+
+		If an app directory is specified or it has a default, it becomes the active directory before tests are run.
 
 		Null characters are allowed in stdin (see: --fork-stdin)
 
 
 		Options:
-		  -q|--quiet         Only the execution of tests and --help will write to stdout/stderr
-		  -p|--params VAL    Contains IFS seperated param(s) to use with all test files, ex: -p '-c=3 -f /my/file'
-		  -F|--fork-stdin    Write stdin into all tests
-		  -e|--fail-exit     Exit on first failed test
-		  --dry-run          Print the filepaths to be executed
+		  -q|--quiet          Only the execution of tests and --help will write to stdout/stderr
+		  -p|--params VAL     Contains IFS seperated param(s) to use with all test files, ex: -p '-c=3 -f /my/file'
+		  -F|--fork-stdin     Write stdin into all tests
+		  -e|--fail-exit      Exit on first failed test
+		  -a|--app-root-dir   Root directory of the application to cd into prior to running tests
+		  --dry-run           Print the filepaths to be executed
 
 
 		Examples:
@@ -46,7 +51,6 @@ help_doc() {
 	HelpDoc
 	[[ $1 ]] && exit "$1"
 }
-[[ $1 ]] || help_doc 0
 
 
 
@@ -55,6 +59,7 @@ quiet=
 test_params=()
 fork_stdin=
 fail_exit=
+app_root_dir=
 dry_run=
 tmp_dir='/tmp'
 
@@ -83,6 +88,8 @@ while [[ $1 ]]; do
 			fork_stdin=1 ;;
 		'--fail-exit'|'-e')
 			fail_exit=1 ;;
+		'--app-root-dir'|'-a')
+			shift; app_root_dir=$1 ;;
 		'--dry-run')
 			dry_run=1 ;;
 		'--help'|'-h')
@@ -100,8 +107,29 @@ test_paths+=("$@")
 
 
 
-# Validate values
-[[ ${#test_files[@]} ]] || help_doc 2
+# Determine absolute directory of this script
+script_dir=$(cd -- "${BASH_SOURCE[0]%/*}"; pwd)
+
+# If the directory in which the script resides is named 'tests'
+if [[ ${script_dir##*/} == 'tests' ]]; then
+
+	# If not app_root_dir is specified the directory one level up is used
+	if [[ ! $app_root_dir ]]; then
+		app_root_dir=${script_dir%/*}
+		: ${app_root_dir:='/'}
+	fi
+
+	# If no test paths are specified, the sub-directories of the script's directory become the test paths
+	[[ ${#test_paths[@]} == 0 ]] && test_paths=("$script_dir"'/'*'/')
+
+else
+	[[ ${#test_paths[@]} == 0 ]] && print_stderr 1 '%s\n' 'No test paths given'
+
+fi
+
+
+
+# Validate directories
 [[ -d $tmp_dir ]] || printf '%s\n' 'temp directory doesnt exist: '"$tmp_dir"
 
 
@@ -110,6 +138,8 @@ test_paths+=("$@")
 shopt -s nullglob globstar
 test_files=()
 for test_path in "${test_paths[@]}"; do
+
+	test_path=$(cd -- "$PWD/$test_path"; pwd)
 
 	[[ -e $test_path ]] || print_stderr 4 '%s\n' 'test path does not exist: '"$test_path"
 	[[ -x $test_path ]] || print_stderr 4 '%s\n' 'test path is not executable: '"$test_path"
@@ -155,6 +185,7 @@ fi
 
 
 # Execute tests
+cd -- "$app_root_dir"
 test_failed=
 for test_path in "${test_files[@]}"; do
 
